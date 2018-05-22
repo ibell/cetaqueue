361ed474ba72c23e49ed134b59c232341c5ed368
Ian Bell <ian.h.bell@gmail.com>
2018-05-21 19:34:20 -0600

    Single producer single consumer working

--- checkdb.py
+++ checkdb.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 import gridfs
 import io
 import zipfile
+import time
 
 if __name__=='__main__':
 
@@ -19,12 +20,16 @@ if __name__=='__main__':
 
     fs = gridfs.GridFS(db)
     _id = fs.put(b'something wicked this way comes...')
+    
+    while True:
 
-    # See https://stackoverflow.com/a/44946732/1360263  
-    zip_buffer = io.BytesIO()
-    with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zip_file:
-        for file_name, data in [('1.txt', io.BytesIO(b'111')), ('2.txt', io.BytesIO(b'222'))]:
-            zip_file.writestr(file_name, data.getvalue())
-        
-    _id = fs.put(zip_buffer.getvalue(), filename='result.zip')
-    print('Zip result pushed')
+        # See https://stackoverflow.com/a/44946732/1360263  
+        zip_buffer = io.BytesIO()
+        with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zip_file:
+            for file_name, data in [('1.txt', io.BytesIO(b'1'*10000)), ('2.txt', io.BytesIO(b'222'))]:
+                zip_file.writestr(file_name, data.getvalue())
+            
+        _id = fs.put(zip_buffer.getvalue(), filename='result.zip')
+        print('Zip result pushed to db @ '+str(_id))
+
+        time.sleep(1.0)
