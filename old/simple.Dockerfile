0d1059acfe767471903a371f743b0358396d4717
Ian Bell <ian.h.bell@gmail.com>
2018-06-01 23:56:18 -0600

    a bit more cleanup

--- old/simple.Dockerfile
+++ old/simple.Dockerfile
@@ -1,3 +1,3 @@
 FROM continuumio/miniconda3
 COPY simple.py .
-CMD id -u && id -g && ls -al / && ls -al /output/ && touch /output/hello.txt && ls -al /output/ && python -u simple.py && ls -al /output/ && ls -al /mount && ls -al /mount/job
+CMD python -u simple.py
--- worker/worker_entrypoint.sh
+++ worker/worker_entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 set -e
-#ls -al /
-#ls -al /mount
-mkdir /mount/job
-chown -Rv baleen /mount/job
-ls -ld /mount/job
-ln -s /mount/job /output
-ls -al /mount
-ls -al /
-ls -al /output
+mkdir /mount/job && chown -Rv baleen /mount/job && ln -s /mount/job /output
 # mount
 exec gosu baleen "$@"
