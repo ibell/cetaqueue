deadf4ba2dd40f5da952e7de5f09a9b5f96449e0
Ian Bell <ian.h.bell@gmail.com>
2018-06-09 13:36:37 -0600

    add ability to push more than one job at once

--- old/push.py
+++ old/push.py
@@ -1,16 +1,24 @@
-import requests, os, zipfile
+import requests, os, zipfile, argparse
 url = 'http://localhost:5000/add_job'
-
-with zipfile.ZipFile(os.path.join(os.path.dirname(__file__),'data.zip'), 'w') as myzip:
-    myzip.write(os.path.join(os.path.dirname(__file__),'simple.py'), arcname='simple.py')
 
 zname = os.path.join(os.path.dirname(__file__),'data.zip')
 dname = os.path.join(os.path.dirname(__file__),'simple.Dockerfile')
 
-files = {'file': ('data.zip', open(zname, 'rb'), 'application/zip')}
-Dockerfile = open(dname).read()
-r = requests.post(url, files=files, data= dict(Dockerfile=Dockerfile))
-print(r.text)
 
-if os.path.exists(zname):
-    os.remove(zname)
+if __name__=='__main__':
+    
+    parser = argparse.ArgumentParser(description='Push some simple jobs')
+    parser.add_argument('N', type=int, nargs=1, help='an integer for the accumulator',default=1)
+    args = parser.parse_args()
+
+    for _ in range(args.N[0]):
+    
+        with zipfile.ZipFile(zname, 'w') as myzip:
+            myzip.write(os.path.join(os.path.dirname(__file__),'simple.py'), arcname='simple.py')
+
+        files = {'file': ('data.zip', open(zname, 'rb'), 'application/zip')}
+        Dockerfile = open(dname).read()
+        r = requests.post(url, files=files, data= dict(Dockerfile=Dockerfile))
+
+        if os.path.exists(zname):
+            os.remove(zname)
